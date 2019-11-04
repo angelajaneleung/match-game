54601a7338acadc296f9596f306e5a73ea41a389
Angela Jane Leung <angelajaneleung@gmail.com>
2019-11-04 18:53:07 -0500

    hopefully it will work on GitHub pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as ServiceWorker from './ServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+ServiceWorker.unregister();
