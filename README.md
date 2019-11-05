0b0597c94849beeda636bdf45e1e48fed35be9f0
Angela Jane Leung <angelajaneleung@gmail.com>
2019-11-05 18:53:36 -0500

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Doggo Memory Game 
-
-
-# How to Play
-Click on the dog pics  for points! But if you click on the same picture more than once, you lose. See if you can remember all 15 without double clicking on any, if so, you win!
-
-
-
---
+++ precache-manifest.73ba1a627f5eb6de98f92f2b26aed01e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9839bdceadb38361d9",
+    "url": "/static/css/main.6d8eaf3e.chunk.css"
+  },
+  {
+    "revision": "3f9839bdceadb38361d9",
+    "url": "/static/js/main.3f9839bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "81fa42650c0b162ca1d5",
+    "url": "/static/js/2.81fa4265.chunk.js"
+  },
+  {
+    "revision": "8146deb9e311838b29d0b21aef49698b",
+    "url": "/static/media/dog1.8146deb9.jpg"
+  },
+  {
+    "revision": "d3ee9681ae49ef236272bcb79de1847d",
+    "url": "/static/media/dog10.d3ee9681.jpg"
+  },
+  {
+    "revision": "d759d3338e2ed0cdfe82024058cd8c73",
+    "url": "/static/media/dog11.d759d333.jpg"
+  },
+  {
+    "revision": "8316c3d03731cc67e569e68d95a92644",
+    "url": "/static/media/dog12.8316c3d0.jpg"
+  },
+  {
+    "revision": "a4d08f5800bd5df704cf71f779e66dca",
+    "url": "/static/media/dog13.a4d08f58.jpg"
+  },
+  {
+    "revision": "166439ee2bad7e89f133b00f156f587a",
+    "url": "/static/media/dog14.166439ee.jpg"
+  },
+  {
+    "revision": "ce1f2bfb12161e8e134aaa01e556e0e5",
+    "url": "/static/media/dog15.ce1f2bfb.jpg"
+  },
+  {
+    "revision": "d6ee8f0203cb891032c1675a9c8e26b0",
+    "url": "/static/media/dog2.d6ee8f02.jpg"
+  },
+  {
+    "revision": "2fbf640309b4e1a2e7f22afa04b34286",
+    "url": "/static/media/dog3.2fbf6403.jpg"
+  },
+  {
+    "revision": "d13c0c885723474ebfcbe6d701eef1b8",
+    "url": "/static/media/dog4.d13c0c88.jpg"
+  },
+  {
+    "revision": "ec88c026a46a89a17ff59058ba76c4fb",
+    "url": "/static/media/dog5.ec88c026.jpg"
+  },
+  {
+    "revision": "db6e99693e46bb2e752f2124be3e2834",
+    "url": "/static/media/dog6.db6e9969.jpg"
+  },
+  {
+    "revision": "c6e9ecc17db38a5a7784aa6c94a2d683",
+    "url": "/static/media/dog7.c6e9ecc1.jpg"
+  },
+  {
+    "revision": "19fdfebdff01418720e5d4b668d7b74f",
+    "url": "/static/media/dog8.19fdfebd.jpg"
+  },
+  {
+    "revision": "c313cfdd9f779226c51aaf33f3f93844",
+    "url": "/static/media/dog9.c313cfdd.jpg"
+  },
+  {
+    "revision": "8783ce2900b86eb292fdc218eed4f78c",
+    "url": "/index.html"
+  }
+];
