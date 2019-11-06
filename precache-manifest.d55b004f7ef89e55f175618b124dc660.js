ebe1e67dec9c979f325c51898d29fe4facb395d9
Angela Jane Leung <angelajaneleung@gmail.com>
2019-11-05 19:00:27 -0500

    Updates

---
+++ precache-manifest.d55b004f7ef89e55f175618b124dc660.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "95b4a0424a88d4999a21",
+    "url": "/match-game/static/css/main.6d8eaf3e.chunk.css"
+  },
+  {
+    "revision": "95b4a0424a88d4999a21",
+    "url": "/match-game/static/js/main.95b4a042.chunk.js"
+  },
+  {
+    "revision": "c4976bd67326d315be4f",
+    "url": "/match-game/static/js/runtime~main.c4976bd6.js"
+  },
+  {
+    "revision": "81fa42650c0b162ca1d5",
+    "url": "/match-game/static/js/2.81fa4265.chunk.js"
+  },
+  {
+    "revision": "8146deb9e311838b29d0b21aef49698b",
+    "url": "/match-game/static/media/dog1.8146deb9.jpg"
+  },
+  {
+    "revision": "d3ee9681ae49ef236272bcb79de1847d",
+    "url": "/match-game/static/media/dog10.d3ee9681.jpg"
+  },
+  {
+    "revision": "d759d3338e2ed0cdfe82024058cd8c73",
+    "url": "/match-game/static/media/dog11.d759d333.jpg"
+  },
+  {
+    "revision": "8316c3d03731cc67e569e68d95a92644",
+    "url": "/match-game/static/media/dog12.8316c3d0.jpg"
+  },
+  {
+    "revision": "a4d08f5800bd5df704cf71f779e66dca",
+    "url": "/match-game/static/media/dog13.a4d08f58.jpg"
+  },
+  {
+    "revision": "166439ee2bad7e89f133b00f156f587a",
+    "url": "/match-game/static/media/dog14.166439ee.jpg"
+  },
+  {
+    "revision": "ce1f2bfb12161e8e134aaa01e556e0e5",
+    "url": "/match-game/static/media/dog15.ce1f2bfb.jpg"
+  },
+  {
+    "revision": "d6ee8f0203cb891032c1675a9c8e26b0",
+    "url": "/match-game/static/media/dog2.d6ee8f02.jpg"
+  },
+  {
+    "revision": "2fbf640309b4e1a2e7f22afa04b34286",
+    "url": "/match-game/static/media/dog3.2fbf6403.jpg"
+  },
+  {
+    "revision": "d13c0c885723474ebfcbe6d701eef1b8",
+    "url": "/match-game/static/media/dog4.d13c0c88.jpg"
+  },
+  {
+    "revision": "ec88c026a46a89a17ff59058ba76c4fb",
+    "url": "/match-game/static/media/dog5.ec88c026.jpg"
+  },
+  {
+    "revision": "db6e99693e46bb2e752f2124be3e2834",
+    "url": "/match-game/static/media/dog6.db6e9969.jpg"
+  },
+  {
+    "revision": "c6e9ecc17db38a5a7784aa6c94a2d683",
+    "url": "/match-game/static/media/dog7.c6e9ecc1.jpg"
+  },
+  {
+    "revision": "19fdfebdff01418720e5d4b668d7b74f",
+    "url": "/match-game/static/media/dog8.19fdfebd.jpg"
+  },
+  {
+    "revision": "c313cfdd9f779226c51aaf33f3f93844",
+    "url": "/match-game/static/media/dog9.c313cfdd.jpg"
+  },
+  {
+    "revision": "3bdf13df48942665a620e5c60a578089",
+    "url": "/match-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73ba1a627f5eb6de98f92f2b26aed01e.js"
+  "/match-game/precache-manifest.d55b004f7ef89e55f175618b124dc660.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/match-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
